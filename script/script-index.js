da62be287e45a5f4dc3f83dd1fe0e356b4853ca3
Luiz Felipe Föeger <luizffsan5@gmail.com>
2024-10-05 16:07:22 -0300

    Estilização da sessão "Meus Projetos
    Estilização da sessão Meus Projetos mobile

--- script/script-index.js
+++ script/script-index.js
@@ -84,25 +84,3 @@ nextButton.addEventListener('click', () => {
     currentIndex = (currentIndex < items.length - 1) ? currentIndex + 1 : 0;
     updateCarousel();
 });
-
-
-const carouselInnerMobile = document.querySelector('.carousel-inner-mobile');
-const itemsMobile = document.querySelectorAll('.carousel-item-mobile');
-const prevButtonMobile = document.getElementById('prev');
-const nextButtonMobile = document.getElementById('next');
-let currentIndexMobile = 0;
-
-function updateCarouselMobile() {
-    const offset = -currentIndexMobile * 100;
-    carouselInnerMobile.style.transform = `translateX(${offset}%)`;
-}
-
-prevButtonMobile.addEventListener('click', () => {
-    currentIndexMobile = (currentIndexMobile > 0) ? currentIndexMobile - 1 : itemsMobile.length - 1;
-    updateCarouselMobile();
-});
-
-nextButtonMobile.addEventListener('click', () => {
-    currentIndexMobile = (currentIndexMobile < itemsMobile.length - 1) ? currentIndexMobile + 1 : 0;
-    updateCarouselMobile();
-});
