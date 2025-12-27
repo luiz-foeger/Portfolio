85d1d1b2043a9a76efd6770b89267405f92cc0ff
Luiz Felipe Föeger <luizffsan5@gmail.com>
2025-12-27 00:03:50 -0300

    adding images to project cards

--- src/components/common/Projects.tsx
+++ src/components/common/Projects.tsx
@@ -12,7 +12,7 @@ const projects = [
         tags: ["WordPress", "Elementor", "WooCommerce"],
         link: "#",
         github: "",
-        src: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop"
+        src: "https://res.cloudinary.com/db43xalo3/image/upload/v1766803724/Opera_Instant%C3%A2neo_2025-12-26_232210_iutute.png"
     },
     {
         title: "CineDev",
@@ -20,7 +20,7 @@ const projects = [
         tags: ["React", "JavaScript", "CSS", "API Integration"],
         link: "https://cinedev-project.vercel.app",
         github: "https://github.com/luiz-foeger/CineDev",
-        src: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop"
+        src: "https://res.cloudinary.com/db43xalo3/image/upload/v1766803494/Opera_Instant%C3%A2neo_2025-12-26_232210_ywekyc.png"
     },
     {
         title: "Street&Wear",
@@ -28,16 +28,24 @@ const projects = [
         tags: ["React", "JavaScript", "CSS"],
         link: "https://streetwear-project.vercel.app",
         github: "https://github.com/luiz-foeger/StreetWear",
-        src: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop"
+        src: "https://res.cloudinary.com/db43xalo3/image/upload/v1766800107/937bb523-0ec2-4ab7-8365-ea6ed0557761.png"
     },
     {
-        title: "BarberHub",
-        description: "Uma plataforma completa para agendamento de serviços em barbearias. Conecta clientes a profissionais da região de Santa Teresa, com gestão de horários em tempo real e sistema de avaliação.",
-        tags: ["React", "JavaScript", "CSS", "Python"],
-        link: "https://barber-hub.vercel.app",
-        github: "",
-        src: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop"
+        title: "EA SPORTS FC™ 24",
+        description: "Landing page para o jogo EA SPORTS FC 24. Destaque para a arquitetura da informação, focados em apresentar o conteúdo de forma imersiva e direta, utilizando elementos visuais de alta qualidade e CTAs claras.",
+        tags: ["HTML", "JavaScript", "CSS"],
+        link: "https://streetwear-project.vercel.app",
+        github: "https://github.com/luiz-foeger/StreetWear",
+        src: "https://res.cloudinary.com/db43xalo3/image/upload/v1766803785/6ba57bac-e44e-459f-bdfd-1957ee72d4f4.png"
     },
+    // {
+    //     title: "BarberHub",
+    //     description: "Uma plataforma completa para agendamento de serviços em barbearias. Conecta clientes a profissionais da região de Santa Teresa, com gestão de horários em tempo real e sistema de avaliação.",
+    //     tags: ["React", "JavaScript", "CSS", "Python"],
+    //     link: "https://barber-hub.vercel.app",
+    //     github: "",
+    //     src: "https://images.unsplash.com/photo-1498050108023-c5249f4df085?q=80&w=2072&auto=format&fit=crop"
+    // },
 ];
 
 // --- COMPONENTE DO CARD INDIVIDUAL ---
@@ -101,7 +109,8 @@ const Card = ({
                                 ))}
                             </div>
 
-                            <p className="text-sm md:text-lg text-white/80 leading-relaxed mb-4 md:mb-8">{description}</p>                        </div>
+                            <p className="text-sm md:text-lg text-white/80 leading-relaxed mb-4 md:mb-8">{description}</p>
+                        </div>
 
                         <div className="flex items-center gap-6">
                             {github && (
@@ -117,12 +126,12 @@ const Card = ({
                     </div>
 
                     {/* LADO DIREITO */}
-                    <div className="w-full md:w-[55%] flex-1 md:h-full min-h-[200px] rounded-2xl overflow-hidden relative border border-white/10 z-10 bg-black/50 order-1 md:order-2 shadow-xl">
+                    <div className="w-full md:w-[55%] flex-1 md:h-full min-h-[200px] rounded-2xl overflow-hidden relative border border-white/10 z-10 order-1 md:order-2 bg-[#111] pt-10 pl-6 pr-6  shadow-xl">
                         <motion.div style={{ scale: imageScale }} className="w-full h-full relative">
                             <img
                                 src={src}
                                 alt={`Imagem do projeto ${title}`}
-                                className="object-cover w-full h-full opacity-90 hover:opacity-100 transition-opacity duration-500"
+                                className="object-cover object-top w-full h-full opacity-90 hover:opacity-100 transition-opacity duration-500 rounded-t-lg"
                             />
                         </motion.div>
                     </div>
@@ -143,8 +152,6 @@ const Projects = () => {
     return (
         <section ref={container} id="projetos" className="relative mt-32 mb-32">
 
-            {/* --- AMBIENT LIGHT (Opcional, mas recomendado) --- */}
-            {/* Uma luz azul sutil atrás dos cards para o vidro ter o que refletir */}
             <div className="absolute top-[20%] left-[50%] -translate-x-1/2 w-[80%] h-[60%] bg-[#8dcffb] opacity-10 blur-[120px] rounded-full z-0 pointer-events-none"></div>
 
             <div className="max-w-[1000px] mx-auto px-6 mb-24 relative z-10">
@@ -154,9 +161,9 @@ const Projects = () => {
                     viewport={{ once: true }}
                     className="text-white font-bold text-3xl md:text-6xl text-center mb-8 md:mb-12 tracking-tight"
                 >
-                    Projetos Principais
+                    Projetos Destaques
                 </motion.h2>
-                <motion.p className="text-white/90 text-sm sm:text-base md:text-2xl leading-relaxed">
+                <motion.p className="text-white/90 text-sm sm:text-base md:text-2xl leading-relaxed text-center max-w-3xl mx-auto">
                     Uma coleção de projetos que desafiaram minhas habilidades de design e código.
                 </motion.p>
             </div>
